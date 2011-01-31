44e472068b163b7be001b74fec1ed98bcdde85e2
Erkki Seppälä <erkki.seppala@vincit.fi>
2011-01-31 14:02:01 +0200

    ximcp/imRm: Handle leaking colormap_ret
    Fixed memory leak by adding Xfree for colormap_ret
    
    Variable "colormap_ret" goes out of scope
    
    Reviewed-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Ander Conselvan de Oliveira <ander.conselvan-de-oliveira@nokia.com>
    Signed-off-by: Erkki Seppälä <erkki.seppala@vincit.fi>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Backported-to-NX-by: Ulrich Sibiller <uli42@gmx.de>

--- nx-X11/lib/X11/imRm.c
+++ nx-X11/lib/X11/imRm.c
@@ -2808,6 +2808,8 @@ _XimEncodeLocalPreeditValue(
 				ic->core.focus_window, &colormap_ret,
 				&count, (Atom)p->value)))
 	    return False;
+
+	Xfree(colormap_ret);
     }
     return True;
 }
@@ -2828,6 +2830,8 @@ _XimEncodeLocalStatusValue(
 				ic->core.focus_window, &colormap_ret,
 				&count, (Atom)p->value)))
 	    return False;
+
+	Xfree(colormap_ret);
     }
     return True;
 }
