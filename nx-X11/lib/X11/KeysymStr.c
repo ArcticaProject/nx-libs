62e5b0ff80f91a66163386e2192bcb8e0b2d1721
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2011-09-22 18:44:13 -0400

    makekeys: Fix build/target word size mismatch when cross-compiling
    Since makekeys is built using build environment's gcc and
    runs natively, we have to make sure that the size of the
    Signature type is the same on both the native environment
    and the target, otherwise we get mismatches upon running X,
    and some LSB test failures (xts5).
    
    Use an unsigned 32-bit integer on all platforms. Also,
    eliminate the redundant multiple typedefs for the
    Signature type.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>
    Reviewed-by: Daniel Stone <daniel@fooishbar.org>

--- nx-X11/lib/X11/KeysymStr.c
+++ nx-X11/lib/X11/KeysymStr.c
@@ -31,10 +31,9 @@ in this Software without prior written authorization from The Open Group.
 #include "Xlibint.h"
 #include <nx-X11/Xresource.h>
 #include <nx-X11/keysymdef.h>
+#include "Xresinternal.h"
 
 #include <stdio.h> /* sprintf */
-
-typedef unsigned long Signature;
 
 #define NEEDVTABLE
 #include "ks_tables.h"
