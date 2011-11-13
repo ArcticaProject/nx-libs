6dc46faed83596d64bb86d9a4b35543f5122e1dd
Reinhard Tartler <siretart@tauware.de>
2011-11-13 09:53:38 +0100

    Imported nxproxy-3.5.0-1.tar.gz
    Summary: Imported nxproxy-3.5.0-1.tar.gz
    Keywords:
    
    Imported nxproxy-3.5.0-1.tar.gz
    into Git repository

--- nxproxy/Main.c
+++ nxproxy/Main.c
@@ -1,6 +1,6 @@
 /**************************************************************************/
 /*                                                                        */
-/* Copyright (c) 2001, 2007 NoMachine, http://www.nomachine.com/.         */
+/* Copyright (c) 2001, 2011 NoMachine, http://www.nomachine.com/.         */
 /*                                                                        */
 /* NXPROXY, NX protocol compression and NX extensions to this software    */
 /* are copyright of NoMachine. Redistribution and use of the present      */
