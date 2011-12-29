21a85ffbe8d97fd7d5d71149360afca13f842878
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2011-12-29 14:45:18 +0100

    Make sure upstream changelog file names are in lower case.

--- debian/pre-build-nx.sh
+++ debian/pre-build-nx.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# create copies of upstream changelog whose names apply to Debian policy...
+cp -a nx-X11/CHANGELOG nx-X11/changelog
+cp -a nxcomp/CHANGELOG nxcomp/changelog
+cp -a nxcompext/CHANGELOG nxcompext/changelog
+cp -a nxcompshad/CHANGELOG nxcompshad/changelog
+cp -a nx-X11/programs/Xserver/hw/nxagent/CHANGELOG nx-X11/programs/Xserver/hw/nxagent/changelog
+cp -a nx-X11/programs/nxauth/CHANGELOG nx-X11/programs/nxauth/changelog
+cp -a nxproxy/CHANGELOG nxproxy/changelog
+
 exit 0
