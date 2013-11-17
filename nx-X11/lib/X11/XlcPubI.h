35dc4784bcee7a42a83cc1dbab13fb82cfcc5803
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-11-16 20:21:54 -0800

    Drop X_LOCALE fallback for OS'es without setlocale()
    C89 or bust!   This was documented as being needed for "only Lynx,
    Linux-libc5, OS/2" and has never been enabled in modular builds,
    since none of those platforms have had anyone step up to add support
    since the X11R7 conversion to autotools.
    
    Mostly performed with unifdef -UX_LOCALE, followed by removal of files
    left without any purpose, and manual cleanup of remaining references.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Backported-to-NX-by: Ulrich Sibiller <uli42@gmx.de>

--- nx-X11/lib/X11/XlcPubI.h
+++ nx-X11/lib/X11/XlcPubI.h
@@ -232,12 +232,6 @@ extern void _XlcGetLocaleDataBase(
     int*		/* count */
 );
 
-#ifdef X_LOCALE
-extern char *
-_Xsetlocale(
-    int           category,
-    _Xconst char  *name);
-#else
 #if defined(__APPLE__) || defined(__DARWIN__)
 extern char *
 _Xsetlocale(
@@ -247,7 +241,6 @@ _Xsetlocale(
 extern char *_XlcMapOSLocaleName(
     char *osname,
     char *siname);
-#endif
 
 extern int
 _Xmbstoutf8(
--- nx-X11/lib/X11/Xlocale.h
+++ nx-X11/lib/X11/Xlocale.h
@@ -33,28 +33,6 @@ from The Open Group.
 #include <nx-X11/Xfuncproto.h>
 #include <nx-X11/Xosdefs.h>
 
-#ifndef X_LOCALE
 #include <locale.h>
-#else
-
-#define LC_ALL      0
-#define LC_COLLATE  1
-#define LC_CTYPE    2
-#define LC_MONETARY 3
-#define LC_NUMERIC  4
-#define LC_TIME     5
-
-_XFUNCPROTOBEGIN
-extern char *_Xsetlocale(
-    int /* category */,
-    _Xconst char* /* name */
-);
-_XFUNCPROTOEND
-
-#define setlocale _Xsetlocale
-
-#include <stddef.h>
-
-#endif /* X_LOCALE */
 
 #endif /* !defined(_X11_XLOCALE_H_) && !defined(_XLOCALE_H_) */
