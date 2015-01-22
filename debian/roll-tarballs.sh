be832e8e15a4a783431ef2d87c8eb4cab918635c
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2015-01-22 06:12:10 +0100

    Drop more unused code in nx-X11/programs/Xserver/hw/. Do this in roll-tarball.sh and in debian/rules alike.

--- debian/roll-tarballs.sh
+++ debian/roll-tarballs.sh
@@ -140,6 +140,7 @@ rm -Rf "debian/"
 rm -Rf nx-X11/extras/{drm,expat,fontconfig,freetype2,fonts,ogl-sample,regex,rman,ttf2pt1,x86emu,zlib}
 rm -Rf nx-X11/lib/{expat,fontconfig,fontenc,font/FreeType,font/include/fontenc.h,freetype2,regex,zlib}
 rm -Rf nx-X11/lib/{FS,ICE,SM,Xaw,Xft,Xt,Xmu,Xmuu}
+rm -Rf nx-X11/programs/Xserver/hw/{darwin,dmx,kdrive,sun,sunLynx,vfb,xnest,xwin}
 
 # remove files, that we do not want in the tarballs (build cruft)
 rm -Rf nx*/configure nx*/autom4te.cache*
