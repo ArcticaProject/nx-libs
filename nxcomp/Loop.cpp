a8fb7d4f5149e28216ffd38838af1cbfd6527002
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2015-02-13 13:38:39 +0100

    Detection for Mac OS X's launchd service on Mac OS X 10.10 and beyond (607_nxcomp_macosx-X11-launcher-in-private-tmp.full+lite.patch).

--- nxcomp/Loop.cpp
+++ nxcomp/Loop.cpp
@@ -4183,7 +4183,7 @@ int SetupDisplaySocket(int &xServerAddrFamily, sockaddr *&xServerAddr,
 
   #ifdef __APPLE__
 
-  if (strncasecmp(display, "/tmp/launch", 11) == 0)
+  if ((strncasecmp(display, "/tmp/launch", 11) == 0) || (strncasecmp(display, "/private/tmp/com.apple.launchd", 30) == 0))
   {
     #ifdef TEST
     *logofs << "Loop: Using launchd service on socket '"
