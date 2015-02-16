5fc2f57fb5520bb61e2c1f8b6fd2522b203b3b9d
Mihai Moldovan <ionic@ionic.de>
2015-02-16 05:26:40 +0100

    Revert "CVE-2014-0210: unvalidated length fields in fs_read_query_info() from xorg/lib/libXfont commit 491291cabf78efdeec8f18b09e14726a9030cc8f"
    This reverts commit c6aebf9284855a0e24ad9c5ffdd36aa65e16bec7.

--- nx-X11/lib/font/fc/fserve.c
+++ nx-X11/lib/font/fc/fserve.c
@@ -866,7 +866,6 @@ fs_read_query_info(FontPathElementPtr fpe, FSBlockDataPtr blockrec)
     FSFpePtr		conn = (FSFpePtr) fpe->private;
     fsQueryXInfoReply	*rep;
     char		*buf;
-    long		bufleft; /* length of reply left to use */
     fsPropInfo		*pi;
     fsPropOffset	*po;
     pointer		pd;
@@ -897,10 +896,7 @@ fs_read_query_info(FontPathElementPtr fpe, FSBlockDataPtr blockrec)
 
     buf = (char *) rep;
     buf += SIZEOF(fsQueryXInfoReply);
- 
-    bufleft = rep->length << 2;
-    bufleft -= SIZEOF(fsQueryXInfoReply);
-
+    
     /* move the data over */
     fsUnpack_XFontInfoHeader(rep, pInfo);
     
@@ -908,51 +904,19 @@ fs_read_query_info(FontPathElementPtr fpe, FSBlockDataPtr blockrec)
     _fs_init_fontinfo(conn, pInfo);
 
     /* Compute offsets into the reply */
-    if (bufleft < SIZEOF(fsPropInfo))
-    {
-	ret = -1;
-#ifdef DEBUG
-	fprintf(stderr, "fsQueryXInfo: bufleft (%ld) < SIZEOF(fsPropInfo)\n",
-		bufleft);
-#endif
-	goto bail;
-    }
     pi = (fsPropInfo *) buf;
     buf += SIZEOF (fsPropInfo);
-    bufleft -= pi->num_offsets * SIZEOF(fsPropOffset);
     
-    if (bufleft < pi->data_len)
-    {
-	ret = -1;
-#ifdef DEBUG
-	fprintf(stderr,
-		"fsQueryXInfo: bufleft (%ld) < data_len (%d)\n",
-		bufleft, pi->data_len);
-#endif
-	goto bail;
-    }
     po = (fsPropOffset *) buf;
     buf += pi->num_offsets * SIZEOF(fsPropOffset);
-    bufleft -= pi->data_len;
 
-    {
-	ret = -1;
-#ifdef DEBUG
-	fprintf(stderr,
-		"fsQueryXInfo: bufleft (%ld) < data_len (%d)\n",
-		bufleft, pi->data_len);
-#endif
-	goto bail;
-    }
     pd = (pointer) buf;
     buf += pi->data_len;
-    bufleft -= pi->data_len;
     
     /* convert the properties and step over the reply */
     ret = _fs_convert_props(pi, po, pd, pInfo);
-  bail:    
     _fs_done_read (conn, rep->length << 2);
-
+    
     if (ret == -1)
     {
 	fs_cleanup_bfont (bfont);
