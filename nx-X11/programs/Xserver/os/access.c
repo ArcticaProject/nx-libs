03a2922d9cc17af26bd91d4a471061c54db50789
Mihai Moldovan <ionic@ionic.de>
2015-02-16 05:55:23 +0100

    Revert "dix: integer overflow in GetHosts() [CVE-2014-8092 2/4]"
    This reverts commit d4c76981f7fddb364166464c571ed8d3de3086cd.

--- nx-X11/programs/Xserver/os/access.c
+++ nx-X11/programs/Xserver/os/access.c
@@ -1719,10 +1719,6 @@ GetHosts (
     {
 	nHosts++;
 	n += (((host->len + 3) >> 2) << 2) + sizeof(xHostEntry);
-        /* Could check for INT_MAX, but in reality having more than 1mb of
-           hostnames in the access list is ridiculous */
-        if (n >= 1048576)
-            break;
     }
     if (n)
     {
@@ -1734,8 +1730,6 @@ GetHosts (
         for (host = validhosts; host; host = host->next)
 	{
 	    len = host->len;
-            if ((ptr + sizeof(xHostEntry) + len) > (data + n))
-                break;
 	    ((xHostEntry *)ptr)->family = host->family;
 	    ((xHostEntry *)ptr)->length = len;
 	    ptr += sizeof(xHostEntry);
