f706d774b4dfd4c4254c3cf5c29ec28eb9313568
Salvador Fandino <sfandino@yahoo.com>
2015-06-02 15:35:50 +0200

    remove now unused function ParseListenOption

--- nxcomp/Loop.cpp
+++ nxcomp/Loop.cpp
@@ -524,14 +524,6 @@ static int ParseHostOption(const char *opt, char *host, int &port);
 static int ParseFontPath(char *path);
 
 //
-// Determine the interface where to listen for
-// the remote proxy connection or the local
-// forwarder.
-//
-
-static int ParseListenOption(int &interface);
-
-//
 // Translate a pack method id in a literal.
 //
 
@@ -13474,18 +13466,6 @@ ParseFontPathError:
   #endif
 
   return -1;
-}
-
-int ParseListenOption(int &address)
-{
-  if (loopbackBind || (control->ProxyMode == proxy_server)) {
-    address = htonl(INADDR_LOOPBACK);
-  }
-  else
-  {
-    address = htonl(INADDR_ANY);
-  }
-  return 1;
 }
 
 int OpenLogFile(char *name, ostream *&stream)
