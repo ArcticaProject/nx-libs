2777b5839d1ef8828d4581639bfd002d8015ee92
Nito Martinez <Nito@Qindel.ES>
2016-05-31 10:51:45 +0200

    display version number in nxcomp log, fixes #127

--- nxcomp/Control.cpp
+++ nxcomp/Control.cpp
@@ -607,10 +607,12 @@ Control::Control()
   LocalVersionPatch = NXPatchVersion();
   LocalVersionMaintenancePatch = NXMaintenancePatchVersion();
 
+  *logofs << "Control: NXcomp version is " << NXVersion() << "\n";
   #ifdef TEST
   *logofs << "Control: Major version is " << LocalVersionMajor
           << " minor is " << LocalVersionMinor << " patch is "
-          << LocalVersionPatch << ".\n" << logofs_flush;
+          << LocalVersionPatch << " Maintenance version is "
+	  << LocalVersionMaintenancePatch << ".\n" << logofs_flush;
   #endif
 
   //
