3553595a022f3ec59c485b80856c31049e294488
Ulrich Sibiller <uli42@gmx.de>
2016-07-16 01:35:44 +0200

    replace extensions/composite*.h by system header files

--- nx-X11/programs/Xserver/composite/compint.h
+++ nx-X11/programs/Xserver/composite/compint.h
@@ -50,7 +50,7 @@
 #include "damage.h"
 #include "damageextint.h"
 #include "xfixes.h"
-#include <nx-X11/extensions/compositeproto.h>
+#include <X11/extensions/compositeproto.h>
 #include "compositeext.h"
 #include <assert.h>
 
