b8fe3c7c41749bb7da932ce837bbfb801b8b2742
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2017-02-20 12:54:14 +0100

    Mesa subtree: Adapt build process for building against Mesa that gets pulled in via git subtree.

---
+++ nx-X11/programs/Xserver/GL/mesa/main/WSDrawBuffer.h
@@ -0,0 +1,27 @@
+/**************************************************************************/
+/*                                                                        */
+/* Copyright (c) 2001, 2011 NoMachine (http://www.nomachine.com)          */
+/* Copyright (c) 2008-2014 Oleksandr Shneyder <o.shneyder@phoca-gmbh.de>  */
+/* Copyright (c) 2011-2016 Mike Gabriel <mike.gabriel@das-netzwerkteam.de>*/
+/* Copyright (c) 2014-2016 Mihai Moldovan <ionic@ionic.de>                */
+/* Copyright (c) 2014-2016 Ulrich Sibiller <uli42@gmx.de>                 */
+/* Copyright (c) 2015-2016 Qindel Group (http://www.qindel.com)           */
+/*                                                                        */
+/* NXAGENT, NX protocol compression and NX extensions to this software    */
+/* are copyright of the aforementioned persons and companies.             */
+/*                                                                        */
+/* Redistribution and use of the present software is allowed according    */
+/* to terms specified in the file LICENSE which comes in the source       */
+/* distribution.                                                          */
+/*                                                                        */
+/* All rights reserved.                                                   */
+/*                                                                        */
+/**************************************************************************/
+
+typedef struct _WSDrawBufferRec {
+  GLframebuffer    *DrawBuffer;
+  struct _WSDrawBufferRec *next;
+} WSDrawBufferRec, *WSDrawBufferPtr;
+
+WSDrawBufferPtr pWSDrawBuffer;
+
