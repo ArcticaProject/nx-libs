ArcticaProject/nx-libs

commit 913fcf1a74426725f14380dd5b34286a21c37ab7

Fix some memory leaks.

doc/libNX_X11/lcUniConv/8bit_tab_to_h.c
@@ -121,9 +121,15 @@ int main (int argc, char *argv[])
 
     {
       char* fname = malloc(strlen(directory)+strlen(filename)+1);
+      if (fname == NULL)
+      {
+        printf("malloc failed\n");
+        exit(1);
+      }
       strcpy(fname,directory); strcat(fname,filename);
       f = fopen(fname,"w");
       if (f == NULL)
+        free(fname);
         exit(1);
     }
 

nx-X11/lib/include/xtrans/Xtrans.c
@@ -980,16 +980,17 @@ TRANS(GetMyAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
     {
        prmsg (1,"GetMyAddr: malloc failed\n");
        return -1;
+    } else {
+       memcpy(*addrp, ciptr->addr, ciptr->addrlen);
+       free(addrp);
     }
-    memcpy(*addrp, ciptr->addr, ciptr->addrlen);
-
     return 0;
 }
 #endif
 
 int
 TRANS(GetPeerAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
-		    Xtransaddr **addrp)
+                 Xtransaddr **addrp)
 
 {
     prmsg (2,"GetPeerAddr(%d)\n", ciptr->fd);
@@ -999,11 +1000,11 @@ TRANS(GetPeerAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
 
     if ((*addrp = malloc (ciptr->peeraddrlen)) == NULL)
     {
-	prmsg (1,"GetPeerAddr: malloc failed\n");
-	return -1;
+        prmsg (1,"GetPeerAddr: malloc failed\n");
+        return -1;
     }
     memcpy(*addrp, ciptr->peeraddr, ciptr->peeraddrlen);
-
+    free(addrp);
     return 0;
 }
 

nx-X11/lib/include/xtrans/Xtranssock.c
@@ -2442,19 +2442,18 @@ SocketUNIXConnectPost:
        (ciptr->peeraddr = malloc(namelen)) == NULL)
     {
         prmsg (1,
-	"SocketUNIXCreateListener: Can't allocate space for the addr\n");
+        "SocketUNIXCreateListener: Can't allocate space for the addr\n");
         return TRANS_CONNECT_FAILED;
     }
 
     if (abstract)
-	sockname.sun_path[0] = '@';
+        sockname.sun_path[0] = '@';
 
     ciptr->family = AF_UNIX;
     ciptr->addrlen = namelen;
     ciptr->peeraddrlen = namelen;
     memcpy (ciptr->addr, &sockname, ciptr->addrlen);
     memcpy (ciptr->peeraddr, &sockname, ciptr->peeraddrlen);
-
     return 0;
 }
 

nx-X11/programs/Xserver/Xi/exevents.c
@@ -656,6 +656,7 @@ AddExtensionClient(WindowPtr pWin, ClientPtr client, Mask mask, int mskidx)
     if (!others)
 	return BadAlloc;
     if (!pWin->optional->inputMasks && !MakeInputMasks(pWin))
+	free(others);
 	return BadAlloc;
     bzero((char *)&others->mask[0], sizeof(Mask) * EMASKSIZE);
     others->mask[mskidx] = mask;

nx-X11/programs/Xserver/hw/nxagent/NXmiexpose.c
@@ -629,11 +629,13 @@ int what;
 	    screenContext[i] = CreateGC((DrawablePtr)pWin, (BITS32) 0,
 					(XID *)NULL, &status);
 	    if (!screenContext[i])
+		free(prect);
 		return;
 	    numGCs++;
 	    if (!AddResource(FakeClientID(0), ResType,
-			     (void *)screenContext[i]))
-	        return;
+				    (void *)screenContext[i]))
+		free(prect);
+		return;
 	}
 	pGC = screenContext[i];
 	newValues[SUBWINDOW].val = IncludeInferiors;

nx-X11/programs/Xserver/hw/nxagent/NXrender.c
@@ -995,6 +995,7 @@ ProcRenderCompositeGlyphs (ClientPtr client)
     {
 	listsBase = (GlyphListPtr) malloc (nlist * sizeof (GlyphListRec));
 	if (!listsBase)
+	    free(glyphsBase);
 	    return BadAlloc;
     }
 

nx-X11/programs/Xserver/hw/nxagent/compext/Png.c
@@ -283,6 +283,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
                 bitsPerPixel);
     #endif
 
+    free(image_index);
     return NULL;
   }
 
@@ -315,6 +316,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
     fprintf(stderr, "******PngCompressData: PANIC! Failed creating the png_create_write_struct.\n");
     #endif
 
+    free(image_index);
     return NULL;
   }
 
@@ -327,7 +329,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
     #endif
 
     png_destroy_write_struct(&png_ptr, NULL);
-
+    free(image_index);
     return NULL;
   }
 
@@ -339,6 +341,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
 
     png_destroy_write_struct(&png_ptr, &info_ptr);
 
+    free(image_index);
     return NULL;
   }
 
@@ -360,6 +363,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
                 PNG_DEST_SIZE(w, h));
     #endif
 
+    free(image_index);
     return NULL;
   }
 
@@ -374,7 +378,6 @@ char *PngCompressData(XImage *image, int *compressed_size)
     png_destroy_write_struct(&png_ptr, &info_ptr);
 
     free(pngCompBuf);
-
     return NULL;
   }
 
@@ -470,7 +473,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
     png_destroy_write_struct(&png_ptr, &info_ptr);
 
     free(pngCompBuf);
-
+    free(image_index);
     return NULL;
   }
 
@@ -484,7 +487,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
       fprintf(stderr, "******PngCompressData: PANIC! Cannot allocate [%d] bytes.\n",
                   (int) (w * sizeof(CARD8)));
       #endif
-
+      free(image_index);
       return NULL;
     }
 
@@ -516,7 +519,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
     #endif
 
     free(pngCompBuf);
-
+    free(image_index);
     return NULL;
   }
 

nx-X11/programs/Xserver/mi/miexpose.c
@@ -766,11 +766,13 @@ int what;
 	    screenContext[i] = CreateGC((DrawablePtr)pWin, (BITS32) 0,
 					(XID *)NULL, &status);
 	    if (!screenContext[i])
+		free(prect);
 		return;
 	    numGCs++;
 	    if (!AddResource(FakeClientID(0), ResType,
 			     (void *)screenContext[i]))
-	        return;
+		free(prect);
+		return;
 	}
 	pGC = screenContext[i];
 	newValues[SUBWINDOW].val = IncludeInferiors;

commit 992673157077080f3d5637c7f666e43639181a91

misc: fix more regressions and whitespace weirdness introduced in 913fcf1a74426725f14380dd5b34286a21c37ab7.

doc/libNX_X11/lcUniConv/8bit_tab_to_h.c
@@ -129,8 +129,10 @@ int main (int argc, char *argv[])
       strcpy(fname,directory); strcat(fname,filename);
       f = fopen(fname,"w");
       if (f == NULL)
+      {
         free(fname);
         exit(1);
+      }
     }
 
     fprintf(f, "\n");

nx-X11/lib/include/xtrans/Xtrans.c
@@ -980,17 +980,17 @@ TRANS(GetMyAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
     {
        prmsg (1,"GetMyAddr: malloc failed\n");
        return -1;
-    } else {
-       memcpy(*addrp, ciptr->addr, ciptr->addrlen);
-       free(addrp);
     }
+
+    memcpy(*addrp, ciptr->addr, ciptr->addrlen);
+
     return 0;
 }
 #endif
 
 int
 TRANS(GetPeerAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
-                 Xtransaddr **addrp)
+                   Xtransaddr **addrp)
 
 {
     prmsg (2,"GetPeerAddr(%d)\n", ciptr->fd);
@@ -1003,7 +1003,9 @@ TRANS(GetPeerAddr) (XtransConnInfo ciptr, int *familyp, int *addrlenp,
         prmsg (1,"GetPeerAddr: malloc failed\n");
         return -1;
     }
+
     memcpy(*addrp, ciptr->peeraddr, ciptr->peeraddrlen);
+
     return 0;
 }
 

nx-X11/lib/include/xtrans/Xtranssock.c
@@ -2442,7 +2442,7 @@ SocketUNIXConnectPost:
        (ciptr->peeraddr = malloc(namelen)) == NULL)
     {
         prmsg (1,
-        "SocketUNIXCreateListener: Can't allocate space for the addr\n");
+       "SocketUNIXCreateListener: Can't allocate space for the addr\n");
         return TRANS_CONNECT_FAILED;
     }
 
@@ -2454,6 +2454,7 @@ SocketUNIXConnectPost:
     ciptr->peeraddrlen = namelen;
     memcpy (ciptr->addr, &sockname, ciptr->addrlen);
     memcpy (ciptr->peeraddr, &sockname, ciptr->peeraddrlen);
+
     return 0;
 }
 

nx-X11/programs/Xserver/Xi/exevents.c
@@ -655,9 +655,10 @@ AddExtensionClient(WindowPtr pWin, ClientPtr client, Mask mask, int mskidx)
     others = (InputClients *) malloc(sizeof(InputClients));
     if (!others)
 	return BadAlloc;
-    if (!pWin->optional->inputMasks && !MakeInputMasks(pWin))
+    if (!pWin->optional->inputMasks && !MakeInputMasks(pWin)) {
 	free(others);
 	return BadAlloc;
+    }
     bzero((char *)&others->mask[0], sizeof(Mask) * EMASKSIZE);
     others->mask[mskidx] = mask;
     others->resource = FakeClientID(client->index);

nx-X11/programs/Xserver/hw/nxagent/NXmiexpose.c
@@ -628,14 +628,17 @@ int what;
 		return;
 	    screenContext[i] = CreateGC((DrawablePtr)pWin, (BITS32) 0,
 					(XID *)NULL, &status);
-	    if (!screenContext[i])
+	    if (!screenContext[i]) {
 		free(prect);
 		return;
+	    }
 	    numGCs++;
 	    if (!AddResource(FakeClientID(0), ResType,
-				    (void *)screenContext[i]))
+			     (void *)screenContext[i]))
+	    {
 		free(prect);
 		return;
+	    }
 	}
 	pGC = screenContext[i];
 	newValues[SUBWINDOW].val = IncludeInferiors;

nx-X11/programs/Xserver/hw/nxagent/NXrender.c
@@ -995,8 +995,10 @@ ProcRenderCompositeGlyphs (ClientPtr client)
     {
 	listsBase = (GlyphListPtr) malloc (nlist * sizeof (GlyphListRec));
 	if (!listsBase)
+	{
 	    free(glyphsBase);
 	    return BadAlloc;
+	}
     }
 
     elementsBase = malloc(nlist * sizeof(XGlyphElt8));

nx-X11/programs/Xserver/hw/nxagent/compext/Png.c
@@ -330,6 +330,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
 
     png_destroy_write_struct(&png_ptr, NULL);
     free(image_index);
+
     return NULL;
   }
 
@@ -340,8 +341,8 @@ char *PngCompressData(XImage *image, int *compressed_size)
     #endif
 
     png_destroy_write_struct(&png_ptr, &info_ptr);
-
     free(image_index);
+
     return NULL;
   }
 
@@ -364,6 +365,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
     #endif
 
     free(image_index);
+
     return NULL;
   }
 
@@ -376,8 +378,8 @@ char *PngCompressData(XImage *image, int *compressed_size)
     #endif
 
     png_destroy_write_struct(&png_ptr, &info_ptr);
-
     free(pngCompBuf);
+
     return NULL;
   }
 
@@ -474,6 +476,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
 
     free(pngCompBuf);
     free(image_index);
+
     return NULL;
   }
 
@@ -487,7 +490,9 @@ char *PngCompressData(XImage *image, int *compressed_size)
       fprintf(stderr, "******PngCompressData: PANIC! Cannot allocate [%d] bytes.\n",
                   (int) (w * sizeof(CARD8)));
       #endif
+
       free(image_index);
+
       return NULL;
     }
 
@@ -520,6 +525,7 @@ char *PngCompressData(XImage *image, int *compressed_size)
 
     free(pngCompBuf);
     free(image_index);
+
     return NULL;
   }
 

nx-X11/programs/Xserver/mi/miexpose.c
@@ -766,13 +766,17 @@ int what;
 	    screenContext[i] = CreateGC((DrawablePtr)pWin, (BITS32) 0,
 					(XID *)NULL, &status);
 	    if (!screenContext[i])
+	    {
 		free(prect);
 		return;
+	    }
 	    numGCs++;
 	    if (!AddResource(FakeClientID(0), ResType,
 			     (void *)screenContext[i]))
+	    {
 		free(prect);
 		return;
+	    }
 	}
 	pGC = screenContext[i];
 	newValues[SUBWINDOW].val = IncludeInferiors;

nx-X11/programs/Xserver/mi/mizerline.c
@@ -158,9 +158,12 @@ miZeroLine(pDraw, pGC, mode, npt, pptInit)
     pspanInit = (DDXPointPtr)malloc(list_len * sizeof(DDXPointRec));
     pwidthInit = (int *)malloc(list_len * sizeof(int));
     if (!pspanInit || !pwidthInit)
+    {
         free(pspanInit);
         free(pwidthInit);
+
         return;
+    }
 
     Nspans = 0;
     new_span = TRUE;

nx-X11/programs/Xserver/render/render.c
@@ -1346,9 +1346,12 @@ ProcRenderCompositeGlyphs (ClientPtr client)
     {
 	listsBase = (GlyphListPtr) malloc (nlist * sizeof (GlyphListRec));
 	if (!listsBase)
+	{
 	    free(glyphsBase);
 	    free(listsBase);
+
 	    return BadAlloc;
+	}
     }
     buffer = (CARD8 *) (stuff + 1);
     glyphs = glyphsBase;
@@ -2920,6 +2923,8 @@ PanoramiXRenderFillRectangles (ClientPtr client)
 	    result = (*PanoramiXSaveRenderVector[X_RenderFillRectangles]) (client);
 	    if(result != Success) break;
 	}
+
+	free(extra);
     }
     return result;
 }
@@ -2979,9 +2984,10 @@ PanoramiXRenderTrapezoids(ClientPtr client)
 
 	    if(result != Success) break;
 	}
+
+	free(extra);
     }
 
-    free(extra);
     return result;
 }
 
@@ -3036,9 +3042,10 @@ PanoramiXRenderTriangles(ClientPtr client)
 
 	    if(result != Success) break;
 	}
-	
+
+	free(extra);
     }
-    free(extra);
+
     return result;
 }
 
@@ -3089,9 +3096,10 @@ PanoramiXRenderTriStrip(ClientPtr client)
 
 	    if(result != Success) break;
 	}
-	
+
+	free(extra);
     }
-    free(extra);
+
     return result;
 }
 
@@ -3142,9 +3150,10 @@ PanoramiXRenderTriFan(ClientPtr client)
 
 	    if(result != Success) break;
 	}
-	
+
+	free(extra);
     }
-    free(extra);
+
     return result;
 }
 
@@ -3272,8 +3281,10 @@ PanoramiXRenderAddTraps (ClientPtr client)
 	    result = (*PanoramiXSaveRenderVector[X_RenderAddTraps]) (client);
 	    if(result != Success) break;
 	}
+
+	free(extra);
     }
-    free(extra);
+
     return result;
 }
 