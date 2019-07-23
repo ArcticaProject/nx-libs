17e0265a49466acfcef1da026e22a79f5a89ba6d
Ulrich Sibiller <uli42@gmx.de>
2019-07-23 21:31:40 +0200

    Screen.c: fix: use bit operator, not logic operator

--- nx-X11/programs/Xserver/hw/nxagent/Screen.c
+++ nx-X11/programs/Xserver/hw/nxagent/Screen.c
@@ -4694,7 +4694,7 @@ void nxagentPrintGeometry(void)
 
   for (i = 0; i < screenInfo.numScreens; i++)
   {
-    if (nxagentPrintGeometryFlags && (1 << i))
+    if (nxagentPrintGeometryFlags & (1 << i))
     {
       fprintf(stderr, "Info: Screen [%d] resized to geometry [%dx%d] "
                   "fullscreen [%d].\n", i, screenInfo.screens[i] -> width,
