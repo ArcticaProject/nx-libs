8b796ee54b2b750c81d987637e9fb2ef2f871c76
Ulrich Sibiller <uli42@gmx.de>
2019-10-23 00:19:09 +0200

    Binder.c: reformat comments

--- nx-X11/programs/Xserver/hw/nxagent/Binder.c
+++ nx-X11/programs/Xserver/hw/nxagent/Binder.c
@@ -49,8 +49,7 @@ int nxagentCheckBinder(int argc, char *argv[], int i)
     char *display = argv[i];
 
     /*
-     * Check if a display specification follows
-     * the -B switch.
+     * Check if a display specification follows the -B switch.
      */
 
     char *found = rindex(display, ':');
@@ -73,8 +72,8 @@ int nxagentCheckBinder(int argc, char *argv[], int i)
 
     /*
      * The NX options must be specified in the DISPLAY
-     * environment. Check if the display specified on
-     * the command line matches the NX virtual display.
+     * environment. Check if the display specified on the command line
+     * matches the NX virtual display.
      */
 
     display = getenv("DISPLAY");
@@ -103,8 +102,8 @@ int nxagentCheckBinder(int argc, char *argv[], int i)
     #endif
 
     /*
-     * Save the proxy options. They will be later
-     * used to create the transport.
+     * Save the proxy options. They will be later used to create the
+     * transport.
      */
 
     nxagentChangeOption(Rootless, False);
@@ -112,10 +111,9 @@ int nxagentCheckBinder(int argc, char *argv[], int i)
     nxagentChangeOption(Binder, True);
 
     /*
-     * FIXME: This now points to the buffer that was
-     * returned by getenv(). It is to be decided how
-     * to handle the values of type string in the
-     * Options repository.
+     * FIXME: This now points to the buffer that was returned by
+     * getenv(). It is to be decided how to handle the values of type
+     * string in the Options repository.
      */
      
     nxagentChangeOption(BinderOptions, display);
