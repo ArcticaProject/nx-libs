77477916ec895495a96ddfae0392d936710441da
Ulrich Sibiller <uli42@gmx.de>
2024-03-28 22:03:53 +0100

    Pixmap.c: add missing [] to TEST output

--- nx-X11/programs/Xserver/hw/nxagent/Pixmap.c
+++ nx-X11/programs/Xserver/hw/nxagent/Pixmap.c
@@ -244,7 +244,7 @@ PixmapPtr nxagentCreatePixmap(ScreenPtr pScreen, int width, int height,
   }
 
   #ifdef TEST
-  fprintf(stderr, "nxagentCreatePixmap: Allocated memory for the Virtual %sPixmap %p of real Pixmap %p (%dx%d),"
+  fprintf(stderr, "nxagentCreatePixmap: Allocated memory for the Virtual %sPixmap [%p] of real Pixmap [%p] (%dx%d),"
               "allocation hint [%d].\n",
               nxagentShmPixmapTrap ? "Shm " : "", (void *) pVirtual, (void *) pPixmap, width, height, usage_hint);
   #endif
