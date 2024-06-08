5f7c8e73b3eaceb7651957c2701e055286d8aadd
Ulrich Sibiller <uli42@gmx.de>
2024-06-08 17:44:53 +0200

    Render.c: merge masks in nxagentReconnectPicture

--- nx-X11/programs/Xserver/hw/nxagent/Render.c
+++ nx-X11/programs/Xserver/hw/nxagent/Render.c
@@ -2260,19 +2260,11 @@ void nxagentReconnectPicture(void * p0, XID x1, void *p2)
   }
 
   attributes.subwindow_mode = pPicture -> subWindowMode;
-  mask |= CPSubwindowMode;
-
   attributes.poly_edge = pPicture -> polyEdge;
-  mask |= CPPolyEdge;
-
   attributes.poly_mode = pPicture -> polyMode;
-  mask |= CPPolyMode;
-
   attributes.dither = pPicture -> dither;
-  mask |= CPDither;
-
   attributes.component_alpha = pPicture -> componentAlpha;
-  mask |= CPComponentAlpha;
+  mask |= (CPSubwindowMode | CPPolyEdge | CPPolyMode | CPDither | CPComponentAlpha);
 
   XRenderPictFormat *pForm = NULL;
 
